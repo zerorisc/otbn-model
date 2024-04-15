01b294cf8b450a602491af4abce9bfea9457c728
Jade Philipoom <jadep@zerorisc.com>
2024-04-15 17:03:59 +0200

    wip

--- OtbnModel.v
+++ OtbnModel.v
@@ -1,3 +1,4 @@
+Require Import Coq.Strings.String.
 Require Import Coq.Lists.List.
 Require Import Coq.micromega.Lia.
 Require Import Coq.ZArith.ZArith.
@@ -26,6 +27,7 @@ Section Registers.
   | CSR_FG0
   | CSR_FG1
   | CSR_FLAGS
+      (* These registers are not used much in practice.
   | CSR_MOD0
   | CSR_MOD1
   | CSR_MOD2
@@ -34,9 +36,10 @@ Section Registers.
   | CSR_MOD5
   | CSR_MOD6
   | CSR_MOD7
-  | CSR_RND_PREFETCH
   | CSR_RND
   | CSR_URND
+  | CSR_RND_PREFETCH
+       *)
   .
 
   (* Wide special registers, 256 bits each. *)
@@ -103,15 +106,6 @@ Section ISA.
   | Rshi : wdr -> wdr -> wdr -> Z -> Insn
   .
 
-  (* Program graph with control flow. Block constructors are named
-     after the control-flow instruction they end with. *)
-  Inductive Program : Type :=
-  | Ret : list Insn -> Program
-  | Ecall : list Insn -> Program
-  | Jump : list Insn -> Program -> Program
-  | Bne : list Insn -> gpr -> gpr -> Program -> Program -> Program
-  .
-
 End ISA.
 
 (* Notations to interpret assembly code. *)
@@ -135,25 +129,64 @@ Module Notations.
   Notation "'bn.add' d , a , b '<<' s , fg" := (Add d a b (lshift s) fg) (at level 40).
   Notation "'bn.add' d , a , b '>>' s , fg" := (Add d a b (rshift s) fg) (at level 40).
 
+  (*
+  (* Program graph with control flow. Block constructors are named
+     after the control-flow instruction they end with. *)
+  Inductive Block : Type :=
+  | Ret : list Insn -> Block
+  | Ecall : list Insn -> Block
+  (* Tail-call pattern: jal x0, foo *)
+  | JumpNoLink : list Insn -> string -> Block
+  (* Function call pattern: jal x1, foo *)
+  | Call : list Insn -> string -> Block -> Block
+  | Bne : list Insn -> gpr -> gpr -> string -> Block -> Block
+  | LoopEnd : list Insn -> Block (* loop start *) -> Block (* post-loop *) -> Block
+  .
+
   Notation "x ;; 'ecall'" := (Ecall x) (at level 40).
   Notation "x ;; 'ret'" := (Ret x) (at level 40).
-  Notation "x ;; 'jal' 'x1' , foo" := (Jump x foo) (at level 40).
+  Notation "x ;; 'jal' 'x1' , foo ;; y" := (Call x foo y) (at level 60).
+  Notation "x ;; 'jal' 'x0' , foo" := (JumpNoLink x foo) (at level 40).
+  Notation "x ;; 'bne' a , b , foo ;; y" := (Bne x a b foo y) (at level 60).
 
-  (* Test. *)
+  (* Test notations. *)
   Check (bn.add w1, w2, w3 << 0, FG0).
   Check (bn.add w1, w2, w3 >> 3, FG0).
   Check ([bn.add w1, w2, w3 >> 3, FG0; bn.addm w3, w3, w3] ;; ecall).
-  Check (fun p : Program => [bn.add w1, w2, w3 >> 3, FG0; bn.addm w3, w3, w3] ;; jal x1, p).
+  Check ([bn.add w1, w2, w3 >> 3, FG0; bn.addm w3, w3, w3] ;; jal x1, "foo" ;; [] ;; ret).
+  Check ([bn.add w1, w2, w3 >> 3, FG0; bn.addm w3, w3, w3] ;; jal x0, "foo").
+  Check ([bn.add w1, w2, w3 >> 3, FG0; bn.addm w3, w3, w3] ;; bne x1, x2, "foo" ;; [] ;; ret).
+   *)
 End Notations.
 
-(* option monad notation *)
-Definition bind {A B} (x : option A) (f : A -> option B) : option B :=
+(* error monad : either a value or an error message *)
+Definition maybe (A : Type) : Type := A + string.
+Definition bind {A B} (x : maybe A) (f : A -> maybe B) : maybe B :=
   match x with
-  | Some a => f a
-  | None => None
+  | inl a => f a
+  | inr err => inr err
   end.
+Definition map_err {A} (x : option A) (err : string) : maybe A :=
+  match x with
+  | Some v => inl v
+  | None => inr err
+  end.
+Definition prefix_err {A} (x : maybe A) (prefix : string) : maybe A :=
+  match x with
+  | inl v => inl v
+  | inr err => inr (prefix ++ err)%string
+  end.
+Definition maybe_map {A B} (f : A -> B) (x : maybe A) : maybe B :=
+  match x with
+  | inl v => inl (f v)
+  | inr err => inr err
+  end.
+Definition assertion (cond : bool) (msg : string) : maybe unit :=
+  if cond then inl tt else inr msg.
 Local Notation "a <- b ; c" := (bind b (fun a => c)) (at level 100, right associativity).
-       
+Local Notation "'assert!' x msg" := (if x then inr msg%string else inl tt) (at level 40).
+Local Notation Ok := inl (only parsing).
+Local Notation Err := inr (only parsing).
 
 (* bitwise operation shorthand *)
 Local Infix "|'" := Z.lor (at level 40, only parsing) : Z_scope.
@@ -165,8 +198,9 @@ Local Coercion Z.b2z : bool >-> Z.
 (* Executable model of OTBN. *)
 Section Exec.
   (* Parameterize over randomness implementation. *)
-  Context {rnd : nat -> Z}
-    {rnd_range : forall n, 0 <= rnd n < 2^256}.
+  Context {rnd : nat -> Z} {rnd_range : forall x, 0 <= rnd x < 2^256}.
+  Context {urnd : nat -> Z} {urnd_range : forall x, 0 <= urnd x < 2^256}.
+
   (* Parameterize over the map implementation. *)
   Context {map : Type -> Type -> Type}
     {mget : forall {K V}, map K V -> K -> option V}
@@ -176,9 +210,6 @@ Section Exec.
         k1 <> k2 -> mget (mset m k1 v) k2 = mget m k2}.
   Arguments mget {_ _}. Arguments mset {_ _}.
 
-
-  
-
   (* State information for the processor. *)
   Record OtbnState : Type :=
     {
@@ -186,70 +217,204 @@ Section Exec.
       regfile : map reg Z;
       (* Flag states. *)
       flagfile : map flag bool;
-      (* Instruction memory *)
-      imem : map Z Insn;
       (* Data memory *)
       dmem : map Z Z;
       (* Call stack. *)
       callstack : list Z;
-      (* Loop stack. *)
+      (* Loop stack; holds the number of iterations remaining for each loop. *)
       loopstack : list Z;
-      (* Randomness counter (ghost state). *)
-      rnd_counter : nat;
+      (* Instruction counter. *)
+      insn_counter : nat;
     }.
 
-  Definition read_gpr (st : OtbnState) (r : gpr) : OtbnState * option Z :=
+  Definition read_gpr (st : OtbnState) (r : gpr) : maybe Z :=
     match r with
-    | x0 => (st, Some 0)
+    | x0 => Ok 0 (* x0 always reads as 0 *)
     | x1 =>
-        
-        (st', hd_error st.(callstack))
-    | _ => mget st.(regfile) (gpreg r)
+        (* TODO: a direct read from the call stack is possible but
+           rare in practice. For now, don't model it. *)
+        Err "Attempt to directly read from the call stack. This behavior is not yet modelled."%string
+    | _ => map_err (mget st.(regfile) (gpreg r)) "GPR undefined"
     end.
 
-  Definition read_wdr (st : OtbnState) (r : wdr) : option Z := mget st.(regfile) (wdreg r).
-  Print wsr.
-  Definition read_wsr (st : OtbnState) (r : wsr) : option Z :=
+  Definition read_wdr (st : OtbnState) (r : wdr) : maybe Z :=
+    map_err (mget st.(regfile) (wdreg r)) "WDR undefined".
+
+  Definition read_wsr (st : OtbnState) (r : wsr) : maybe Z :=
     match r with
-    | RND => 
+    | RND => Ok (rnd (st.(insn_counter)))
+    | URND => Ok (urnd st.(insn_counter))
+    | _ => map_err (mget st.(regfile) (wsreg r)) "WSR undefined"
+    end.
+
+  Definition read_flag (st : OtbnState) (f : flag) : maybe bool :=
+    map_err (mget st.(flagfile) f) "Flag undefined".
 
   (* Assemble a group of flags into an integer value. *)
-  Definition read_flag_group (st : OtbnState) (fg : flag_group) : option Z :=
-    c <- mget st.(flagfile) (flagC fg) ;
-    m <- mget st.(flagfile) (flagM fg) ;
-    l <- mget st.(flagfile) (flagL fg) ;
-    z <- mget st.(flagfile) (flagZ fg) ;
-    Some (c |' (m << 1) |' (l << 2) |' (z << 3)).    
+  Definition read_flag_group (st : OtbnState) (fg : flag_group) : maybe Z :=
+    c <- read_flag st (flagC fg) ;
+    m <- read_flag st (flagM fg) ;
+    l <- read_flag st (flagL fg) ;
+    z <- read_flag st (flagZ fg) ;
+    Ok (c |' (m << 1) |' (l << 2) |' (z << 3)).
 
-  Print csr.
-  Definition read_csr (st : OtbnState) (r : csr) : option Z :=
+  Definition read_csr (st : OtbnState) (r : csr) : maybe Z :=
     match r with
     | CSR_FG0 => read_flag_group st FG0
     | CSR_FG1 => read_flag_group st FG1
     | CSR_FLAGS =>
         (fg0 <- read_flag_group st FG0;
          fg1 <- read_flag_group st FG1;
-         Some (fg0 |' (fg1 << 4)))
-    | CSR_MOD0 =>
-        (m <- read_wsr st.(regfile) MOD;
-         
+         Ok (fg0 |' (fg1 << 4)))
     end.
-  
 
   (* Implements a read from the register file, handling all special registers. *)
-  Definition read_reg (st : OtbnState) (r : reg) : option Z :=
+  Definition read_reg (st : OtbnState) (r : reg) : maybe Z :=
     match r with
-    | gpreg x0 => Some 0 (* x0 is fixed to zero *)
-    | gpreg x1 =>
-        (* read from x1 pops the call stack *)
-        hd_error st.(callstack)
-    | gpre
-    | csreg r => 
-        
-        
+    | gpreg r => read_gpr st r
+    | wdreg r => read_wdr st r
+    | csreg r => read_csr st r
+    | wsreg r => read_wsr st r
+    end.
 
-  (* Executable version of the processor model (for computation). *)
-  Fixpoint exec (insns : list Insn) : :=
-    
+  Definition read_limb (st : OtbnState) (l : limb) : maybe Z :=
+    match l with
+    | limb0 r => x <- read_wdr st r; Ok (x &' Z.ones 64)
+    | limb1 r => x <- read_wdr st r; Ok ((x >> 64) &' Z.ones 64)
+    | limb2 r => x <- read_wdr st r; Ok ((x >> 128) &' Z.ones 64)
+    | limb3 r => x <- read_wdr st r; Ok ((x >> 192) &' Z.ones 64)
+  end.
+
+  Definition inc_insn_counter (st : OtbnState) : OtbnState :=
+    {| regfile := st.(regfile);
+      flagfile := st.(flagfile);
+      dmem := st.(dmem);
+      callstack := st.(callstack);
+      loopstack := st.(loopstack);
+      insn_counter := st.(insn_counter) + 1;
+    |}.
+
+  Definition regfile_set (st : OtbnState) (r : reg) (v : Z) : OtbnState :=
+    {| regfile := mset st.(regfile) r v;
+      flagfile := st.(flagfile);
+      dmem := st.(dmem);
+      callstack := st.(callstack);
+      loopstack := st.(loopstack);
+      insn_counter := st.(insn_counter);
+    |}.
+
+  Definition cast32 (v : Z) : Z := v &' Z.ones 32.
+  Definition cast256 (v : Z) : Z := v &' Z.ones 256.
+
+  Definition write_gpr (st : OtbnState) (r : gpr) (v : Z) : maybe OtbnState :=
+    match r with
+    | x0 => Ok st
+    | x1 =>
+        (* TODO: this should push to the call stack, but is
+           practically never used. For now, don't model this behavior
+           and treat it as an error. *)
+        Err "Attempt to write to call stack. This behavior is not yet modelled."%string
+    | _ => Ok (regfile_set st (gpreg r) (cast32 v))
+    end.
+
+  Definition write_wdr (st : OtbnState) (r : wdr) (v : Z) : OtbnState :=
+    regfile_set st (wdreg r) (cast256 v).
+  Definition write_wsr (st : OtbnState) (r : wsr) (v : Z) : OtbnState :=
+    match r with
+    | RND => st (* writes to RND are ignored *)
+    | URND => st (* writes to URND are ignored *)
+    | _ => regfile_set st (wsreg r) (cast256 v)
+    end.
+
+  (* mulqacc shifts must be multiples of 64 *)
+  Definition valid_mul_shift (s : Z) : bool :=
+    ((s =? 0)%Z || (s =? 64)%Z || (s =? 128%Z) || (s =? 192%Z))%bool.
+  (* other shift immediates must be multiples of 8 and in the range [0,248] *)
+  Definition valid_arith_shift (s : shift) : bool :=
+    let s := match s with
+             | lshift x => x
+             | rshift x => x
+             end in
+    ((s mod 8 =? 0)%Z && (0 <=? s)%Z && (s <=? 248)%Z)%bool.
+
+  Definition addm_spec (x y m : Z) : Z :=
+    let r := x + y in
+    if r >? m then r - m else r.
+  Definition subm_spec (x y m : Z) : Z :=
+    let r := x - y in
+    if r <? 0 then r + m else r.
+
+  Definition exec1
+    (st : OtbnState) (i : Insn) : maybe OtbnState :=
+    match i with
+    | Mulqacc z x y s =>
+        let st := if z then write_wsr st ACC 0 else st in
+        (_ <- assertion (valid_mul_shift s) "Invalid shift for bn.mulqacc" ;
+         x <- read_limb st x ;
+         y <- read_limb st y ;
+         acc <- read_wsr st ACC ;
+         Ok (write_wsr st ACC (acc + (x * y) << s)))
+    | Mulqacc_wo z d x y s =>
+        let st := if z then write_wsr st ACC 0 else st in
+        (_ <- assertion (valid_mul_shift s) "Invalid shift for bn.mulqacc.wo" ;
+         x <- read_limb st x ;
+         y <- read_limb st y ;
+         acc <- read_wsr st ACC ;
+         let acc' := acc + ((x * y) << s) in
+         Ok (write_wdr (write_wsr st ACC acc') d acc'))
+    | Mulqacc_so z L d x y s =>
+        let st := if z then write_wsr st ACC 0 else st in
+        (_ <- assertion (valid_mul_shift s) "Invalid shift for bn.mulqacc.so" ;
+         x <- read_limb st x ;
+         y <- read_limb st y ;
+         old <- read_wdr st d ;
+         acc <- read_wsr st ACC ;
+         let acc' := acc + ((x * y) << s) in
+         let accL := acc' &' (Z.ones 128) in
+         let accH := acc' >> 128 in
+         let dL := old &' (Z.ones 128) in
+         let dH := old >> 128 in
+         let d' := if L
+                   then (accL |' (dH << 128))
+                   else (dL |' (accL << 128)) in
+         Ok (write_wdr (write_wsr st ACC acc') d d'))
+    | Addm d x y =>
+         (x <- read_wdr st x ;
+          y <- read_wdr st y ;
+          m <- read_wsr st MOD ;          
+          Ok (write_wdr st d (addm_spec x y m)))
+    | Subm d x y =>
+         (x <- read_wdr st x ;
+          y <- read_wdr st y ;
+          m <- read_wsr st MOD ;          
+          Ok (write_wdr st d (subm_spec x y m)))
+    | Add d x y s fg =>
+        (_ <- assertion (valid_arith_shift s) "Invalid shift for bn.add" ;
+         c <- read_flag st (flagC fg) ;
+         x <- read_wdr st x ;
+         y <- read_wdr st y ;
+         Ok (write_wdr st d (x + y + c)))
+    | _ => Err "NotImplemented"%string
+    end.
+
+  Definition exec_straightline
+    (st : OtbnState) (insns : list Insn) : maybe OtbnState :=
+    fold_left (fun st i => (st <- st;
+                            st <- exec1 st i;
+                            Ok (inc_insn_counter st))) insns (Ok st).    
+
+  Print Block.
+  Fixpoint exec
+    (labels : map string Block) (st : OtbnState) (start : Block) : maybe OtbnState :=
+    match start with
+    | Ret insns =>
+        (st <- exec_straightline st insns ;
+         loc <- map_err (hd_error st.(callstack)) "Call stack empty"%string ;
+         exec labels st loc)
+    | _ => Err "NotImplemented"%string
+    end.
+
+  
+
   
 End Exec.
