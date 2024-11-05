5c97e281c940cde06638fb372f0a003bb85046e3
Jade Philipoom <jadep@zerorisc.com>
2024-11-05 15:17:41 +0100

    instantiate all the maps

--- OtbnModel.v
+++ OtbnModel.v
@@ -221,6 +221,74 @@ Section Stringify.
     | csreg r => r
     end.
 
+  Definition wdr_to_string (r : wdr) : string :=
+    match r with
+    | w0 => "w0"
+    | w1 => "w1"
+    | w2 => "w2"
+    | w3 => "w3"
+    | w4 => "w4"
+    | w5 => "w5"
+    | w6 => "w6"
+    | w7 => "w7"
+    | w8 => "w8"
+    | w9 => "w9"
+    | w10 => "w10"
+    | w11 => "w11"
+    | w12 => "w12"
+    | w13 => "w13"
+    | w14 => "w14"
+    | w15 => "w15"
+    | w16 => "w16"
+    | w17 => "w17"
+    | w18 => "w18"
+    | w19 => "w19"
+    | w20 => "w20"
+    | w21 => "w21"
+    | w22 => "w22"
+    | w23 => "w23"
+    | w24 => "w24"
+    | w25 => "w25"
+    | w26 => "w26"
+    | w27 => "w27"
+    | w28 => "w28"
+    | w29 => "w29"
+    | w30 => "w30"
+    | w31 => "w31"
+    end.
+  Local Coercion wdr_to_string : wdr >-> string.
+
+  Definition wsr_to_string (r : wsr) : string :=
+    match r with
+    | WSR_MOD => "WSR_MOD"
+    | WSR_RND => "WSR_RND"
+    | WSR_URND => "WSR_URND"
+    | WSR_ACC => "WSR_ACC"
+    | WSR_KEY_S0_L => "WSR_KEY_S0_L"
+    | WSR_KEY_S0_H => "WSR_KEY_S0_H"
+    | WSR_KEY_S1_L => "WSR_KEY_S1_L"
+    | WSR_KEY_S1_H => "WSR_KEY_S1_H"
+    end.
+  Local Coercion wsr_to_string : wsr >-> string.
+
+  Definition wreg_to_string (r : wreg) : string :=
+    match r with
+    | wdreg r => r
+    | wsreg r => r
+    end.
+
+  Definition flag_to_string (f : flag) : string :=
+    match f with
+    | flagM false => "FG0.M"
+    | flagM true  => "FG1.M"
+    | flagL false => "FG0.L"
+    | flagL true  => "FG1.L"
+    | flagZ false => "FG0.Z"
+    | flagZ true  => "FG1.Z"
+    | flagC false => "FG0.C"
+    | flagC true  => "FG1.C"
+    end.
+
   Definition sinsn_to_string (i : sinsn) : string :=
     match i with
     | Addi rd rs imm =>
@@ -3378,7 +3446,10 @@ Module Test.
 End __.
 End Test.
 
+(* TODO: get index and then use SortedListZ? would also simplify printing and wreg indexing *)
 Require Import coqutil.Word.Naive.
+Require Import coqutil.Map.SortedListWord.
+
 Module SortedListRegs.
   Definition ltb (r1 r2 : reg) := String.ltb (reg_to_string r1) (reg_to_string r2).
   Definition Build_parameters (T : Type) : SortedList.parameters.parameters :=
@@ -3387,19 +3458,66 @@ Module SortedListRegs.
       SortedList.parameters.value := T;
       SortedList.parameters.ltb := ltb;
     |}.
-  Definition reg_strict_order : SortedList.parameters.strict_order ltb.
+  Definition strict_order : SortedList.parameters.strict_order ltb.
   Proof.
-    cbv [ltb reg_to_string gpr_to_string csr_to_string]; constructor; intros.
-    { abstract (repeat destruct_one_match; apply eq_refl). }
-    { abstract (repeat destruct_one_match; try apply eq_refl;
-                repeat destruct_one_match_hyp; assumption). }
-    { abstract (repeat destruct_one_match_hyp; try reflexivity;
+    cbv [ltb]; constructor; intros.
+    { abstract (cbv [reg_to_string gpr_to_string csr_to_string];
+                repeat destruct_one_match; apply eq_refl). }
+    { abstract (eauto using String.ltb_trans). }
+    { abstract (cbv [reg_to_string gpr_to_string csr_to_string] in *;
+                repeat destruct_one_match_hyp; try reflexivity;
                 exfalso; cbv in *; try congruence). }
   Defined.
 
   Global Instance map : map.map reg Naive.word32 :=
-    SortedList.map (Build_parameters _) reg_strict_order.
+    SortedList.map (Build_parameters _) strict_order.
 End SortedListRegs.
+
+Module SortedListWregs.
+  Definition ltb (r1 r2 : wreg) := String.ltb (wreg_to_string r1) (wreg_to_string r2).
+  Definition Build_parameters (T : Type) : SortedList.parameters.parameters :=
+    {|
+      SortedList.parameters.key := wreg;
+      SortedList.parameters.value := T;
+      SortedList.parameters.ltb := ltb;
+    |}.
+  Definition strict_order : SortedList.parameters.strict_order ltb.
+  Proof.
+    cbv [ltb]; constructor; intros.
+    { abstract (cbv [wreg_to_string wdr_to_string wsr_to_string];
+                repeat destruct_one_match; apply eq_refl). }
+    { abstract (eauto using String.ltb_trans). }
+    { abstract (cbv [wreg_to_string wdr_to_string wsr_to_string] in *;
+                repeat destruct_one_match_hyp; try reflexivity;
+                exfalso; cbv in *; try congruence). }
+  Defined.
+
+  Global Instance map : map.map wreg Naive.word256 :=
+    SortedList.map (Build_parameters _) strict_order.
+End SortedListWregs.
+
+Module SortedListFlags.
+  Definition ltb (f1 f2 : flag) := String.ltb (flag_to_string f1) (flag_to_string f2).
+  Definition Build_parameters (T : Type) : SortedList.parameters.parameters :=
+    {|
+      SortedList.parameters.key := flag;
+      SortedList.parameters.value := T;
+      SortedList.parameters.ltb := ltb;
+    |}.
+  Definition strict_order : SortedList.parameters.strict_order ltb.
+  Proof.
+    cbv [ltb]; constructor; intros.
+    { abstract (cbv [flag_to_string];
+                repeat destruct_one_match; apply eq_refl). }
+    { abstract (eauto using String.ltb_trans). }
+    { abstract (cbv [flag_to_string] in *;
+                repeat destruct_one_match_hyp; try reflexivity;
+                exfalso; cbv in *; try congruence). }
+  Defined.
+
+  Global Instance map : map.map flag bool :=
+    SortedList.map (Build_parameters _) strict_order.
+End SortedListFlags.
 
 Module ExecTest.
   (* Check that exec works *)
@@ -3407,6 +3525,8 @@ Module ExecTest.
     (exec1 (regfile:=SortedListRegs.map) (fetch:=fetch Test.prog0) start_state).
   Eval vm_compute in
     (exec (regfile:=SortedListRegs.map) (fetch:=fetch Test.prog0) start_state 100).
+
+  Global Instance mem : map.map Naive.word32 Naive.word32 := SortedListWord.map _ _.
 
   (* scaling factor; create a program with ~n instructions *)
   Definition n := 10000.
@@ -3416,13 +3536,18 @@ Module ExecTest.
       map.empty,
       [ (Addi x3 x0 23 : insn);
         (Jal x1 (fst (fst add_fn)) : insn);
+        (Sw x0 x2 0 : insn);
         (Ecall : insn)])%string.
 
   (* Check that exec completes in a reasonable amount of time *)
   Definition prog : program := ltac:(link_program [start_fn; add_fn]).
   Time
     Eval vm_compute in
-    (exec (regfile:=SortedListRegs.map) (fetch:=fetch prog) start_state (length prog)).
+    (exec (regfile:=SortedListRegs.map)
+       (wregfile:=SortedListWregs.map)
+       (flagfile:=SortedListFlags.map)
+       (fetch:=fetch prog) start_state (length prog)).
+  (* 5.045s *)
 
 End ExecTest.
 
